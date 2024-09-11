8c8f849d5313f52c4ac24054b5fa6df44bca0e89
Kuworld <163482410+Kuworld@users.noreply.github.com>
2024-09-11 16:11:35 +0900

    aa

--- test2/src/main/resources/static/js/slide.js
+++
@@ -1,9 +0,0 @@
- /*슬라이드효과*/
- var counter = 1;
- setInterval(function () {
-   document.getElementById('radio' + counter).checked = true;
-   counter++;
-   if (counter > 2) {
-     counter = 1;
-   }
- }, 4000);
